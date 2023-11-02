e9a50ce1f3bbbf00481eb187a82457be1ca9184a
ShroomyLou <rasmus.holmalahti@gmail.com>
2023-11-02 13:27:46 +0200

    3 LEDS lesgooo
    Added functionality for 3 different colored LEDs.

--- Demos/yet_another_big_sound_demo/yet_another_big_sound_demo.ino
+++ Demos/yet_another_big_sound_demo/yet_another_big_sound_demo.ino
@@ -2,16 +2,24 @@
 // Declaration and initialization of input pins
 int Analog_Input = 1; // Analog output of the sensor
 int Digital_Input = 3; // Digital output of the sensor
-int Led_Output = 4;
-const int sampleWindow =50;
+int LedG_Output = 42;
+int LedY_Output = 43;
+int LedR_Output = 44;
+const int sampleWindow = 50;
 unsigned int sample;
 
+// Setup the pins to use
 void setup  ( )
 {
   pinMode (Analog_Input, INPUT);
   pinMode (Digital_Input, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(Led_Output, OUTPUT);
+  pinMode(LedG_Output, OUTPUT);
+  pinMode(LedY_Output, OUTPUT);
+  pinMode(LedR_Output, OUTPUT);
+  digitalWrite(LedG_Output, LOW);
+  digitalWrite(LedY_Output, LOW);
+  digitalWrite(LedR_Output, LOW);
        
   Serial.begin (115200) ;  //  Serial output with 9600 bps
 }
@@ -20,18 +28,18 @@ void setup  ( )
 // and outputs it on the serial output
 void loop  ( )
 {
-  float  Analog;
+  float Analog;
   float Analog_as_voltage;
   int Digital;
-
   unsigned long startMillis = millis();                  
   float peakToPeak = 0;                                  
   unsigned int signalMax = 0;                            
   unsigned int signalMin = 1024;                  
-    //Current values are read out, converted to the voltage value...
-  Analog =  analogRead (Analog_Input);//   *  (5.0 / 1023.0); 
-  Analog_as_voltage = Analog *  (3.3 / 1023.0);
-  Digital = digitalRead (Digital_Input);   
+  
+  //Current values are read out, converted to the voltage value...
+  Analog =  analogRead(Analog_Input);//   *  (5.0 / 1023.0); 
+  Analog_as_voltage = Analog * (3.3 / 1023.0);
+  Digital = digitalRead(Digital_Input);   
 
   // collect data for 50 mS
   while (millis() - startMillis < sampleWindow)
@@ -54,7 +62,7 @@ void loop  ( )
   Serial.print("sig min");Serial.print(signalMin);
   Serial.print("sig max"); Serial.print(signalMax);
   Serial.print("Current analog sample: "); Serial.print(Analog);
-  //...  and issued at this point
+
   Serial.print  ("Analog voltage value:");  Serial.print (Analog_as_voltage,  4) ;   Serial.print  ("V, ");
   Serial.print("DB: "); Serial.print(db,1);  
   Serial.print ("Limit value:");
@@ -63,16 +71,29 @@ void loop  ( )
   {
       Serial.println ("reached");
       digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(Led_Output, HIGH);
 
   }
   else
   {
       Serial.println (" not yet reached");
       digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(Led_Output, LOW);
 
   }
   Serial.println  ( " ----------------------------------------------------------------") ;
+  else if (db > 60 && db < 85) {
+    digitalWrite(LedG_Output, LOW);
+    digitalWrite(LedY_Output, HIGH);
+    digitalWrite(LedR_Output, LOW);
+  }
+  else if (db >= 85 && db <= 90) {
+    digitalWrite(LedG_Output, LOW);
+    digitalWrite(LedY_Output, LOW);
+    digitalWrite(LedR_Output, HIGH);
+  }
+  else {
+    digitalWrite(LedG_Output, HIGH);
+    digitalWrite(LedY_Output, LOW);
+    digitalWrite(LedR_Output, LOW);
+  }
   delay (200) ;
 }
